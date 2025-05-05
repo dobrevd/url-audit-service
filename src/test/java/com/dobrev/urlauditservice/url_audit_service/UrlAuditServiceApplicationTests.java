bee2c43137413b973ab53161f914b108050d948e
dobrevd <dobrev@ukr.net>
2025-05-05 10:46:55 +0200

    initial commit

---
+++ src/test/java/com/dobrev/urlauditservice/url_audit_service/UrlAuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobrev.urlauditservice.url_audit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlAuditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
